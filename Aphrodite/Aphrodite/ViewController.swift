c9087ae4eb00b65839c5bf76af92836de6e2cf89
Yakun Jin <jinyakun0629@gmail.com>
2018-11-28 19:49:40 +0800

    init Aphrodite

---
+++ Aphrodite/Aphrodite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aphrodite
+//
+//  Created by JK 2018/11/28.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
